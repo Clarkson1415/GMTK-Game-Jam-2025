b736f2e03ed3afda2771b506365020be74ce6b06
Clarkson <graceclarkson1415@gmail.com>
2025-08-07 15:35:09 +1000

    start level 4

--- gmtk2025/GameScenes/GameLevels.gd
+++ gmtk2025/GameScenes/GameLevels.gd
@@ -4,8 +4,9 @@ extends Node
 const _LEVEL_1 = preload("res://GameScenes/Level1.tscn")
 const _LEVEL_2 = preload("res://GameScenes/Level2.tscn")
 const LEVEL_3 = preload("res://GameScenes/Level3.tscn")
+const LEVEL_4 = preload("res://GameScenes/Level4.tscn")
 
-var _levelDictionary: Dictionary[int, PackedScene] = {1: _LEVEL_1, 2: _LEVEL_2, 3: LEVEL_3}
+var _levelDictionary: Dictionary[int, PackedScene] = {1: _LEVEL_1, 2: _LEVEL_2, 3: LEVEL_3, 4: LEVEL_4}
 var currentLevelIndex: int = 1
 
 signal LevelChanged
